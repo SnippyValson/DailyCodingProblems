faf8d0496bc2b44edf4a4dce7a71598fc60d3e43
Snippy Valson <snippy.valson@tismotech.com>
2020-09-24 13:43:09 +0530

    Added problem 3

---
+++ Problem3.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def serialize(node):
+    if node is None:
+        return "Node(NULL)"
+    left_str = serialize(node.left)
+    right_str = serialize(node.right)
+    return "Node({0},{1},{2})".format(node.val, left_str, right_str)
+
+
+def deserialize(nstr):
+    if nstr == "Node(NULL)":
+        return None
+    node_str = nstr[len("Node("):len(nstr)][:-1]
+    comma_index = 0
+    for i in range(0, len(node_str)):
+        if node_str[i] == ',':
+            comma_index = i
+            break
+    val_ = node_str[0:comma_index]
+    level = 0
+    prev_comma_index = comma_index
+    for i in range(comma_index, len(node_str)):
+        if node_str[i] == '(':
+            level = level+1
+        if node_str[i] == ')':
+            level = level-1
+            if level == 0:
+                comma_index = i
+                break
+    left_ = node_str[prev_comma_index+1:comma_index+1]
+    level = 0
+    prev_comma_index = comma_index + 1
+    for i in range(comma_index + 1, len(node_str)):
+        if node_str[i] == '(':
+            level = level+1
+        if node_str[i] == ')':
+            level = level-1
+            if level == 0:
+                comma_index = i
+                break
+    right_ = node_str[prev_comma_index + 1:comma_index + 1]
+    return Node(val_, deserialize(left_), deserialize(right_))
+
+
+root = Node('root', Node('left', Node('left.left')), Node('right'))
+print(serialize(root))
+assert deserialize(serialize(root)).left.left.val == 'left.left'
